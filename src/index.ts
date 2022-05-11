2c24b763f4f76ce0c73c7ccdf40f28628bc36093
唐烨 <tangye@xinpianchang.com>
2022-05-11 15:03:10 +0800

    chore: support nextjs 12 and react 18

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import Router from 'router'
 import Server from 'next/dist/server/next-server'
+import { IncomingMessage, ServerResponse } from 'http'
 
 const router = Router()
 
 const originalHandler = Server.prototype.getRequestHandler
 Server.prototype.getRequestHandler = function getRequestHandler() {
   const handler = originalHandler.call(this)
-  return async (req, res, parsedUrl) => router(req, res, () => handler(req, res, parsedUrl))
+  return async (req, res, parsedUrl) => {
+    if (req instanceof IncomingMessage && res instanceof ServerResponse) {
+      return router(req, res, () => handler(req, res, parsedUrl))
+    }
+    return handler(req, res, parsedUrl)
+  }
 }
 
 export default router
