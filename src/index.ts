dc706b0652ab7df6e4d346d0eb6f29e6af707ba6
唐烨 <tangye@xinpianchang.com>
2022-10-09 11:43:38 +0800

    feat: add events on router to get http server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import Router from 'router'
-import Server from 'next/dist/server/next-server'
-import { IncomingMessage, ServerResponse } from 'http'
+import Router, { Router as NSRouter } from 'router'
+import NextNodeServer from 'next/dist/server/next-server'
+import { IncomingMessage, ServerResponse, type Server as HttpServer } from 'http'
+import EventEmitter from 'events'
 
-const router = Router()
+const router = Router() as NRouter
 
-const originalHandler = Server.prototype.getRequestHandler
-Server.prototype.getRequestHandler = function getRequestHandler() {
+declare module 'next/dist/server/base-server' {
+  interface Options {
+    httpServer?: HttpServer
+  }
+}
+
+export let httpServer: HttpServer | undefined = undefined
+
+const originalHandler = NextNodeServer.prototype.getRequestHandler
+NextNodeServer.prototype.getRequestHandler = function getRequestHandler(this: NextNodeServer) {
   const handler = originalHandler.call(this)
   return async (req, res, parsedUrl) => {
     if (req instanceof IncomingMessage && res instanceof ServerResponse) {
@@ -14,6 +23,27 @@ Server.prototype.getRequestHandler = function getRequestHandler() {
     }
     return handler(req, res, parsedUrl)
   }
+}
+
+const originalPrepare = NextNodeServer.prototype.prepare
+NextNodeServer.prototype.prepare = function prepare(this: NextNodeServer) {
+  httpServer = this.serverOptions.httpServer
+  httpServer && router.events.emit('init', httpServer)
+  return originalPrepare.call(this)
+}
+
+const events = new EventEmitter() as RouterEventEmitter
+router.events = events
+
+interface RouterEventEmitter extends EventEmitter {
+  emit(event: 'init', server: HttpServer): boolean
+  on(event: 'init', listener: (server: HttpServer) => any): this
+  once(event: 'init', listener: (server: HttpServer) => any): this
+  off(event: 'init', listener: (server: HttpServer) => any): this
+}
+
+interface NRouter extends NSRouter.Router {
+  events: RouterEventEmitter
 }
 
 export default router
